faff49662952e39fa300e50d0d5dbb81f35403d3
nurbano <54804496+nurbano@users.noreply.github.com>
2023-03-01 10:23:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 
 """
 # Welcome to Streamlit!
@@ -60,9 +59,15 @@ if option=='aviones.xyz':
         x,y,z,ilum,refle,inte,nb= np.loadtxt('./aviones.xyz',skiprows=1, delimiter=";", unpack=True)
         X=np.column_stack((x, y, z))
         X_filtrada= filtro_aleatorio(X, 5)
-        plt.scatter(X_filtrada[:, 0], X_filtrada[:,1], s= 0.01)
+#         plt.scatter(X_filtrada[:, 0], X_filtrada[:,1], s= 0.01)
 
 #         st.altair_chart(alt.Chart(pd.DataFrame([X_filtrada[:, 0], X_filtrada[:,1]]), height=500, width=500)
 #         .mark_circle(color='#0068c9', opacity=0.5)
 #         .encode(x='x:Q', y='y:Q')) 
+        
+        fig = ff.create_distplot(
+                X, bin_size=[.1, .25, .5])
+
+        # Plot!
+        st.plotly_chart(fig, use_container_width=True)
         
