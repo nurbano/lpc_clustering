299c6fe5daf799f60241b6ea8abd04cfae2df2f6
nurbano <54804496+nurbano@users.noreply.github.com>
2023-03-01 09:23:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,7 @@ option = st.selectbox(
 
 'Seleccion: ', option
 
-match option:
-    case: 'avion.xyz'
+if option=='avion.xyz':
         x,y,z,ilum,refle,inte,nb= np.loadtxt('avion.xyz',skiprows=1, delimiter=";", unpack=True)
         
 st.altair_chart(alt.Chart(pd.DataFrame(x,y), height=500, width=500)
