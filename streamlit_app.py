1e65fc88b1a0c631d2e2ac778457d8b447f30db2
nurbano <54804496+nurbano@users.noreply.github.com>
2023-03-01 11:23:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if option_clustering== "K-means":
     kmeans = KMeans(n_clusters=5).fit(X_filtrada)
     clase_pred=kmeans.labels_
     df = pd.DataFrame(data= {'x': X_filtrada[:, 0], 'y': X_filtrada[:,2], 'inten': X_filtrada[:,3]})
-    fig = px.scatter(data_frame=df, x="x", y="y", color= "inten", title=clustering)
+    fig = px.scatter(data_frame=df, x="x", y="y", color= "inten", title=option_clustering)
     st.plotly_chart(fig)
 
     
