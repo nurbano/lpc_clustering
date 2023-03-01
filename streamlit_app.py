544cb9395e43d9180662396beb0492bc4bdd5040
nurbano <54804496+nurbano@users.noreply.github.com>
2023-03-01 10:40:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import plotly.express as px
-import matplitlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 """
 # Welcome to Streamlit!
